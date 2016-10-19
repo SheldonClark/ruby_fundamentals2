0d92755907bef6fb9938e12502344c4aa5c6fddd
Sheldon Clark <sheldonclark@outlook.com>
2016-10-19 16:44:10 -0400

    take a hash and a percentage increase of students per cohortand increase the number of students per cohort by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -15,3 +15,14 @@ students_per_cohort(students)
 students[:cohort4] = 43
 
 puts students.keys
+
+def more_students(hash, amount_increased)
+	#percentage_increase = amount_increased / 100
+	#puts percentage_increase
+	hash.each do |key,value|
+		new_amount_of_students = value * (1+(amount_increased.to_f/100))
+		hash[key]=new_amount_of_students
+	end
+	students_per_cohort(hash)
+end
+more_students(students, 5)
